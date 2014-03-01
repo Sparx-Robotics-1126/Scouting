3c66f7caf266af224d3b70c21b6a699cc5986568
Justin Bassett <Bassett.JustinT@gmail.com>
2014-02-28 22:04:30 -0500

    Make the view actually good....

--- scouting/src/main/java/com/normandy/aerialassist/scouting/fragments/match/AutoFragment.java
+++ scouting/src/main/java/com/normandy/aerialassist/scouting/fragments/match/AutoFragment.java
@@ -7,7 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.NumberPicker;
+import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -16,28 +17,35 @@ import com.normandy.aerialassist.scouting.R;
 /**
  * Created by jbass on 2/20/14.
  */
-public class AutoFragment extends Fragment implements View.OnClickListener {
+public class AutoFragment extends Fragment implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
-    private static final int REQUEST_CODE = 1126;
+    private static final int START_REQUEST_CODE = 1;
+    private static final int END_REQUEST_CODE = 2;
 
     private FieldLocationSelectionFragment fieldLocationSelectionFragment;
 
     private Button buttonSelectStartLocation;
     private Button buttonSelectEndLocation;
     private Switch switchStartWithBall;
-    private NumberPicker numberPickerBallsAcq;
-    private NumberPicker numberPickerBallsShot;
-    private NumberPicker numberPickerBallsScored;
-    private NumberPicker numberPickerBallsScoredHotHigh;
-    private NumberPicker numberPickerBallsScoredHotLow;
-    private NumberPicker numberPickerBallsScoredHigh;
-    private NumberPicker numberPickerBallsScoredLow;
+
+    private SeekBar seekBarBallsAcq;
+    private SeekBar seekBarBallsShot;
+    private SeekBar seekBarBallsScored;
+    private SeekBar seekBarBallsScoredHotHigh;
+    private SeekBar seekBarBallsScoredHotLow;
+    private SeekBar seekBarBallsScoredHigh;
+    private SeekBar seekBarBallsScoredLow;
+
+    private EditText editTextBallsAcq;
+    private EditText editTextBallsShot;
+    private EditText editTextBallsScored;
+    private EditText editTextBallsScoredHotHigh;
+    private EditText editTextBallsScoredHotLow;
+    private EditText editTextBallsScoredHigh;
+    private EditText editTextBallsScoredLow;
 
     public AutoFragment(){
-        super();
-
         fieldLocationSelectionFragment = new FieldLocationSelectionFragment();
-        fieldLocationSelectionFragment.setTargetFragment(this, REQUEST_CODE);
     }
 
     @Override
@@ -45,39 +53,42 @@ public class AutoFragment extends Fragment implements View.OnClickListener {
         View retVal =  inflater.inflate(R.layout.fragment_auto, container, false);
 
         buttonSelectStartLocation = (Button) retVal.findViewById(R.id.buttonAutoStartLocationSelect);
-        buttonSelectEndLocation = (Button) retVal.findViewById(R.id.buttonAutoEndLocationSelect);
-        switchStartWithBall = (Switch) retVal.findViewById(R.id.switchStartWithBall);
-        numberPickerBallsAcq = (NumberPicker) retVal.findViewById(R.id.numberPickerAutoNumBallsAcq);
-        numberPickerBallsShot = (NumberPicker) retVal.findViewById(R.id.numberPickerAutoNumBallsShot);
-        numberPickerBallsScored  = (NumberPicker) retVal.findViewById(R.id.numberPickerAutoNumBallsScored);
-        numberPickerBallsScoredHotHigh = (NumberPicker) retVal.findViewById(R.id.numberPickerAutoNumBallsHotHigh);
-        numberPickerBallsScoredHotLow = (NumberPicker) retVal.findViewById(R.id.numberPickerAutoNumBallsHotLow);
-        numberPickerBallsScoredHigh = (NumberPicker) retVal.findViewById(R.id.numberPickerAutoNumBallsHigh);
-        numberPickerBallsScoredLow = (NumberPicker) retVal.findViewById(R.id.numberPickerAutoNumBallsLow);
+        buttonSelectEndLocation =   (Button) retVal.findViewById(R.id.buttonAutoEndLocationSelect);
+        switchStartWithBall =       (Switch) retVal.findViewById(R.id.switchStartWithBall);
+
+        seekBarBallsAcq =           (SeekBar) retVal.findViewById(R.id.seekBarAutoNumBallsAcq);
+        seekBarBallsShot =          (SeekBar) retVal.findViewById(R.id.seekBarAutoNumBallsShot);
+        seekBarBallsScored  =       (SeekBar) retVal.findViewById(R.id.seekBarAutoNumBallsScored);
+        seekBarBallsScoredHotHigh = (SeekBar) retVal.findViewById(R.id.seekBarAutoNumBallsHotHigh);
+        seekBarBallsScoredHotLow =  (SeekBar) retVal.findViewById(R.id.seekBarAutoNumBallsHotLow);
+        seekBarBallsScoredHigh =    (SeekBar) retVal.findViewById(R.id.seekBarAutoNumBallsHigh);
+        seekBarBallsScoredLow =     (SeekBar) retVal.findViewById(R.id.seekBarAutoNumBallsLow);
+
+        editTextBallsAcq =           (EditText) retVal.findViewById(R.id.editTextAutoNumBallsAcq);
+        editTextBallsShot =          (EditText) retVal.findViewById(R.id.editTextAutoNumBallsShot);
+        editTextBallsScored  =       (EditText) retVal.findViewById(R.id.editTextAutoNumBallsScored);
+        editTextBallsScoredHotHigh = (EditText) retVal.findViewById(R.id.editTextAutoNumBallsHotHigh);
+        editTextBallsScoredHotLow =  (EditText) retVal.findViewById(R.id.editTextAutoNumBallsHotLow);
+        editTextBallsScoredHigh =    (EditText) retVal.findViewById(R.id.editTextAutoNumBallsHigh);
+        editTextBallsScoredLow =     (EditText) retVal.findViewById(R.id.editTextAutoNumBallsLow);
 
         buttonSelectStartLocation.setOnClickListener(this);
         buttonSelectEndLocation.setOnClickListener(this);
 
-        numberPickerBallsAcq.setMinValue(0);
-        numberPickerBallsAcq.setMaxValue(3);
-
-        numberPickerBallsShot.setMinValue(0);
-        numberPickerBallsShot.setMaxValue(3);
-
-        numberPickerBallsScored.setMinValue(0);
-        numberPickerBallsScored.setMaxValue(3);
-
-        numberPickerBallsScoredHotHigh.setMinValue(0);
-        numberPickerBallsScoredHotHigh.setMaxValue(3);
-
-        numberPickerBallsScoredHotLow.setMinValue(0);
-        numberPickerBallsScoredHotLow.setMaxValue(3);
-
-        numberPickerBallsScoredHigh.setMinValue(0);
-        numberPickerBallsScoredHigh.setMaxValue(3);
-
-        numberPickerBallsScoredLow.setMinValue(0);
-        numberPickerBallsScoredLow.setMaxValue(3);
+        seekBarBallsAcq.setOnSeekBarChangeListener(this);
+        seekBarBallsAcq.setTag(editTextBallsAcq);
+        seekBarBallsShot.setOnSeekBarChangeListener(this);
+        seekBarBallsShot.setTag(editTextBallsShot);
+        seekBarBallsScored.setOnSeekBarChangeListener(this);
+        seekBarBallsScored.setTag(editTextBallsScored);
+        seekBarBallsScoredHotHigh.setOnSeekBarChangeListener(this);
+        seekBarBallsScoredHotHigh.setTag(editTextBallsScoredHotHigh);
+        seekBarBallsScoredHotLow.setOnSeekBarChangeListener(this);
+        seekBarBallsScoredHotLow.setTag(editTextBallsScoredHotLow);
+        seekBarBallsScoredHigh.setOnSeekBarChangeListener(this);
+        seekBarBallsScoredHigh.setTag(editTextBallsScoredHigh);
+        seekBarBallsScoredLow.setOnSeekBarChangeListener(this);
+        seekBarBallsScoredLow.setTag(editTextBallsScoredLow);
 
         return retVal;
     }
@@ -86,10 +97,12 @@ public class AutoFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.buttonAutoStartLocationSelect:
-                fieldLocationSelectionFragment.show(getFragmentManager(), "FieldLocationSelection");
+                fieldLocationSelectionFragment.setTargetFragment(this, START_REQUEST_CODE);
+                fieldLocationSelectionFragment.show(getFragmentManager(), "FieldLocationSelectionStart");
                 break;
             case R.id.buttonAutoEndLocationSelect:
-
+                fieldLocationSelectionFragment.setTargetFragment(this, END_REQUEST_CODE);
+                fieldLocationSelectionFragment.show(getFragmentManager(), "FieldLocationSelectionEnd");
                 break;
             default:
                 throw new RuntimeException("Bad view.");
@@ -99,7 +112,18 @@ public class AutoFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onActivityResult(int requestCode, int responseCode, Intent intent){
         Toast.makeText(getActivity(), "We have returned, responseCode("+responseCode+")", Toast.LENGTH_LONG).show();
-
     }
 
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        EditText et = (EditText) seekBar.getTag();
+        if(et != null)
+            et.setText("" + progress);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {}
 }
