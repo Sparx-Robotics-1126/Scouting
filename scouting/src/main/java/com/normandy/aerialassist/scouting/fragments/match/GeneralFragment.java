4bd58b2e70cde718c34acbe3ed89b02d345109e0
Justin Bassett <Bassett.JustinT@gmail.com>
2014-03-01 13:10:47 -0500

    Attach UI to java General tab

--- scouting/src/main/java/com/normandy/aerialassist/scouting/fragments/match/GeneralFragment.java
+++ scouting/src/main/java/com/normandy/aerialassist/scouting/fragments/match/GeneralFragment.java
@@ -5,18 +5,63 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Switch;
 
 import com.normandy.aerialassist.scouting.R;
 
 /**
  * Created by jbass on 2/20/14.
  */
-public class GeneralFragment extends Fragment{
+public class GeneralFragment extends Fragment implements SeekBar.OnSeekBarChangeListener {
+
+    private Switch switchPlaysDefence;
+
+    private SeekBar seekBarNumberPenalties;
+    private EditText editTextNumberPenalties;
+    private EditText editTextPenaltyComments;
+
+    private SeekBar seekBarNumberTechnicalFouls;
+    private EditText editTextNumberTechnicalFouls;
+    private EditText editTextTechnicalFoulsComments;
+
+    private EditText editTextGeneralComments;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View retVal =  inflater.inflate(R.layout.fragment_general, container, false);
 
+        switchPlaysDefence = (Switch) retVal.findViewById(R.id.switchPlaysDefense);
+
+        seekBarNumberPenalties = (SeekBar) retVal.findViewById(R.id.seekBarPenalties);
+        editTextNumberPenalties = (EditText) retVal.findViewById(R.id.editTextPenalties);
+        editTextPenaltyComments = (EditText) retVal.findViewById(R.id.editTextPenaltyComments);
+        seekBarNumberPenalties.setTag(editTextNumberPenalties);
+        seekBarNumberPenalties.setOnSeekBarChangeListener(this);
+
+
+        seekBarNumberTechnicalFouls = (SeekBar) retVal.findViewById(R.id.seekBarTechnicalFouls);
+        editTextNumberTechnicalFouls = (EditText) retVal.findViewById(R.id.editTextTechnicalFouls);
+        editTextTechnicalFoulsComments= (EditText) retVal.findViewById(R.id.editTextTechnicalFoulComments);
+        seekBarNumberTechnicalFouls.setTag(editTextNumberTechnicalFouls);
+        seekBarNumberTechnicalFouls.setOnSeekBarChangeListener(this);
+
+        editTextGeneralComments = (EditText) retVal.findViewById(R.id.editTextGeneralComments);
+
         return retVal;
     }
 
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        EditText et = (EditText) seekBar.getTag();
+        if(et != null)
+            et.setText(Integer.toString(progress));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {}
 }
