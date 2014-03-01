267a31dae78c8714f50f171f5c1b4b98c9d68e41
Justin Bassett <Bassett.JustinT@gmail.com>
2014-03-01 12:21:18 -0500

    Finish UI for General tab.

--- scouting/src/main/java/com/normandy/aerialassist/scouting/fragments/match/GeneralFragment.java
+++ scouting/src/main/java/com/normandy/aerialassist/scouting/fragments/match/GeneralFragment.java
@@ -1,9 +1,22 @@
 package com.normandy.aerialassist.scouting.fragments.match;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.normandy.aerialassist.scouting.R;
 
 /**
  * Created by jbass on 2/20/14.
  */
 public class GeneralFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View retVal =  inflater.inflate(R.layout.fragment_general, container, false);
+
+        return retVal;
+    }
+
 }
